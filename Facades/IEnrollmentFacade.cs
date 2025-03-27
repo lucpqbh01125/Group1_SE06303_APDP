25c95505a81b9c5df806afc7d6c2cd955f8068bc
TuanAnh0604 <nguyentuananhnt04@gmail.com>
2025-03-28 01:19:27 +0700

    done

--- Facades/IEnrollmentFacade.cs
+++ Facades/IEnrollmentFacade.cs
@@ -1,6 +1,14 @@
-﻿namespace Manager_SIMS.Facades
+﻿using Manager_SIMS.Models;
+
+namespace Manager_SIMS.Facades
 {
     public interface IEnrollmentFacade
     {
+        Task<bool> AssignStudentToCourseAsync(int studentId, int courseId);
+        Task<bool> RemoveEnrollmentAsync(int enrollmentId);
+        Task<List<Enrollment>> GetAllEnrollmentsAsync();
+
+        Task<IEnumerable<Course>> GetStudentCoursesAsync(int studentId);
+
     }
 }
